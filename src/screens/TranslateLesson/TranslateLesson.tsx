49f917c8c780ae964464753a645b23de431d9566
cjeon94 <cjeon94@gmail.com>
2025-08-29 21:28:15 -0700

    Remove placeholder text from input

--- src/screens/TranslateLesson/TranslateLesson.tsx
+++ src/screens/TranslateLesson/TranslateLesson.tsx
@@ -136,7 +136,6 @@ export default function TranslateLesson(): JSX.Element {
                 type="text"
                 value={answer}
                 onChange={(e) => setAnswer(e.target.value)}
-                placeholder="Ana, como estas?"
                 className="w-full h-14 px-4 pr-14 text-lg border-2 border-[#e4e4e4] rounded-xl bg-white shadow-sm focus:border-[#1cb0f6] focus:outline-none transition-colors"
               />
               <button
